41c963ab9f1ffa2dd97a2b15aabd26be6d589322
Ayako <ayakosayama1987@gmail.com>
2019-10-08 11:20:40 -0700

    added firebase database for companies and emploees schedule

--- src/App.js
+++ src/App.js
@@ -5,13 +5,55 @@ import { database } from './firebase';
 
 class App extends React.Component {
 
+  state = {
+    name: '',
+    position: '',
+    email: '',
+    //shcedule
+    title: '',
+    startingTime: '',
+    endingTime: '',
+  }
+
+  updateState = (stateName, value) => {
+    this.setState({
+      [stateName]: value
+    })
+  }
+
   submitInput = () => {
-    database.ref('students').push(
+    const { name, position, email } = this.state;
+    database.ref('companies').child('-LqgNHqFg7rWSC0zWwXJ').child('employees').push(
       //1st parmeter
       {
-        firstname: 'Erika',
-        lastname: 'Miwa',
-        age: 17
+        name: name,
+        position: position,
+        email: email,
+      },
+      //2nd parameter === callback function
+      (err) => {
+        if(err) {
+          console.log('error', err);
+          return
+        }
+        console.log('success!')
+      }
+    )
+  }
+
+  submitSchedule = async () => {
+    const { title, startingTime, endingTime } = this.state;
+
+    const startingTimeDate = await new Date("October 10 2019 16:30").toLocaleString();
+    const endingTimeDate = await new Date("October 10 2019 18:00").toLocaleString();
+
+    console.log('startingtime', startingTimeDate);
+    database.ref('companies').child('-LqgNHqFg7rWSC0zWwXJ').child('employees').child('-LqgQDpmfWbT5TGmFpZD').child('schedule').push(
+      //1st parmeter
+      {
+        title,
+        startingTime: startingTimeDate,
+        endingTime: endingTimeDate,
       },
       //2nd parameter === callback function
       (err) => {
@@ -25,12 +67,12 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    database.ref('students').once('value').then((snapshot) => {
+    database.ref('companies').child('-LqgNHqFg7rWSC0zWwXJ').child('employees').child('-LqgQDpmfWbT5TGmFpZD').child('schedule').child('january2019').once('value').then((snapshot) => {
 
-      snapshot.forEach((eachStudent) => {
-        var childKey = eachStudent.key;
-        var childData = eachStudent.val();
-        console.log('key, value', childKey, childData)
+      snapshot.forEach(eachSchedule => {
+        var childKey = eachSchedule.key;
+        var childData = eachSchedule.val();
+        console.log('key, value', {id: childKey, schedule: childData})
       });
       // var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
     }).catch(err => console.log('error!', err));
@@ -52,7 +94,15 @@ class App extends React.Component {
           >
             Learn React
           </a>
-          <button onClick={this.submitInput}>Submit new user</button>
+          <input placeholder='Name' onChange={(e) => this.updateState('name', e.target.value)} />
+          <input placeholder="position"  onChange={(e) => this.updateState('position', e.target.value)} />
+          <input placeholder="email"  onChange={(e) => this.updateState('email', e.target.value)} />
+          <button onClick={this.submitInput}>Submit New Employee</button>
+          <br/><br/>
+          <input placeholder='Title' onChange={(e) => this.updateState('title', e.target.value)} />
+          <input placeholder="Starting Time"  onChange={(e) => this.updateState('startingTime', e.target.value)} />
+          <input placeholder="Ending Time"  onChange={(e) => this.updateState('endingTime', e.target.value)} />
+          <button onClick={this.submitSchedule}>Submit New Time</button>
         </header>
       </div>
     );
